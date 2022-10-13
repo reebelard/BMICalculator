b5b2a00ad99e15c3f95b13666e7600731be260be
SlyPredator <navneethmahadevan@gmail.com>
2022-10-13 18:55:11 +0530

    refactor(code): move to correct paths

--- BMI_calc.py
+++ BMI_calc.py
@@ -1,18 +1,20 @@
 import tkinter as tk
-from tkinter import ttk
-from PIL import Image, ImageTk
-from tkinter import messagebox
 from pathlib import Path
+from tkinter import messagebox, ttk
+
 import mysql.connector as pymysql
+from PIL import Image, ImageTk
+
+from db_handler import *
 
 ASSETS_PATH = Path(__file__).resolve().parent / "assets"
 
-messagebox.showwarning(
+messagebox.showinfo(
     title="Welcome, User!",
     message="Welcome!\nBody Mass Index (BMI) is widely "
     + "used as an indicator of body fat content.\n",
 )
-messagebox.showwarning(
+messagebox.showinfo(
     title="Welcome, User!",
     message="If you want to calculate your BMI, you have to find out your "
     + "weight and height first. Once you know these values, "
@@ -25,10 +27,11 @@ root.geometry("470x580+300+200")
 root.resizable(False, False)
 root.configure(bg="white")
 root.focus_force()
-
-
+root.bind_all("<Button-1>", lambda event: event.widget.focus_set())
+getval = ""
 # BMI calculation
 def BMI():
+    global getval
     h = float(Height.get())
     w = float(Weight.get())
 
@@ -44,25 +47,35 @@ def BMI():
 
     if bmi <= 18.5:
         label2.config(text="Underweight!")
-        label3.config(text="You weigh lesser than an normal person!")
+        label3.config(
+            text=mycursor_fetch_any("bmi_vals", "bmi_getter", "name = 'Underweight'")[
+                0
+            ][0]
+        )
+        getval = "Underweight"
 
     elif bmi > 18.5 and bmi <= 25:
         label2.config(text="Normal!")
-        label3.config(text="It indicates that you are Healthy!")
+        label3.config(
+            text=mycursor_fetch_any("bmi_vals", "bmi_getter", "name = 'Normal'")[0][0]
+        )
+        getval = "Normal"
 
     elif bmi > 25 and bmi <= 30:
         label2.config(text="Overweight!")
         label3.config(
-            text="It indicates that you are slightly \n Overweight! \n A"
-            + "doctor may advise you to lose some \n weight to be Healthy and "
-            + "risk-free from \n further Health complications."
+            text=mycursor_fetch_any("bmi_vals", "bmi_getter", "name = 'Overweight'")[0][
+                0
+            ]
         )
+        getval = "Overweight"
 
     else:
         label2.config(text="Obese!")
         label3.config(
-            text="Your health may be at risk,\n if you do not " + "lose weight!"
+            text=mycursor_fetch_any("bmi_vals", "bmi_getter", "name = 'Obese'")[0][0]
         )
+        getval = "Obese"
 
 
 # ICON
@@ -97,12 +110,12 @@ def get_current_value():
 def slider_changed(event):
     Height.set(get_current_value())
 
-    size = int(float(get_current_value()))
+    size = int(float(Height.get()))
     img = Image.open(ASSETS_PATH / "Guy.png")
-    resized_image = img.resize((50, 10 + size))
+    resized_image = img.resize((int(float(Weight.get())), 10 + size))
     photo2 = ImageTk.PhotoImage(resized_image)
     secondimage.config(image=photo2)
-    secondimage.place(x=70, y=550 - size)
+    secondimage.place(x=70, y=540 - size)
     secondimage.image = photo2
 
 
@@ -133,6 +146,14 @@ def get_current_value2():
 def slider_changed2(event):
     Weight.set(get_current_value2())
 
+    size = int(float(Weight.get()))
+    img = Image.open(ASSETS_PATH / "Guy.png")
+    resized_image = img.resize((50 + size, 550 - int(float(Height.get()))))
+    photo2 = ImageTk.PhotoImage(resized_image)
+    secondimage.config(image=photo2)
+    secondimage.place(x=70, y=int(float(Height.get())))
+    secondimage.image = photo2
+
 
 def add_data():
     if float(Height.get()) == 0.00 or float(Weight.get()) == 0.00:
@@ -141,25 +162,8 @@ def add_data():
             message="Weight/Height cannot be 0!",
         )
     else:
-        sqlCon = pymysql.connect(
-            host="localhost",
-            user="root",
-            password=enter_password,
-            database=db_name,
-        )
-        cur = sqlCon.cursor()
-        cur.execute(
-            "insert into bmi values(%s, %s)",
-            (
-                Height.get(),
-                Weight.get(),
-            ),
-        )
-        sqlCon.commit()
-        messagebox.showinfo(
-            title="BMI Calculator",
-            message="Data inserted successfully.",
-        )
+        tup = (BMIval.get(), Height.get(), Weight.get(), getval)
+        bmi_update(tup)
 
 
 # COMMAND TO CHANGE BACKGROUND COLOR OF SCALE
@@ -181,6 +185,7 @@ slider2.place(x=300, y=250)
 # ENTRY BOX
 Height = tk.StringVar()
 Weight = tk.StringVar()
+BMIval = tk.StringVar(value="")
 height = tk.Entry(
     root,
     textvariable=Height,
@@ -193,7 +198,6 @@ height = tk.Entry(
 )  # 'justify' TO align text in center
 height.place(x=35, y=160)
 Height.set(get_current_value())
-
 weight = tk.Entry(
     root,
     textvariable=Weight,
@@ -206,6 +210,7 @@ weight = tk.Entry(
 )  # 'justify' TO align text in center
 weight.place(x=255, y=160)
 Weight.set(get_current_value2())
+name_entry = tk.Entry(master=root, width=75, textvariable=BMIval).place(x=20, y=60)
 
 # MAN IMAGE
 secondimage = tk.Label(root, bg="orange")
